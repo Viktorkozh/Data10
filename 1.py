7bf618f59f109a3a76f3d3f311f4ef109eb19089
ViktorKozh <viktor.ru253@gmail.com>
2024-05-17 22:16:39 +0300

    Speedrunn't

---
+++ 1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+from threading import Thread, Barrier
+
+barrier = Barrier(2)
+result_p_0 = []
+result_p_omk = []
+
+
+def P_0(p, N, l):
+    sum_series = sum([(p**k) / math.factorial(k) for k in range(N+1)])
+    extra_term = ((p**(N+1)) / (N * math.factorial(N))) * \
+        ((1 - (p/N)**l) / (1 - p/N))
+    result_p_0.append((sum_series + extra_term)**(-1))
+    barrier.wait()
+
+
+def P_omk(p, N, l):
+    barrier.wait()
+    result_p_omk.append(
+        ((p/N)**l) * ((p**N) / math.factorial(N)) * result_p_0[0])
+
+
+def main():
+    p = 0.25  # интенсивность трафика
+    N = 2     # Количество продавцов
+    l = 5     # Максимум в очереди
+
+    thread1 = Thread(target=P_0, args=(p, N, l))
+    thread2 = Thread(target=P_omk, args=(p, N, l))
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+    print("P_0:", result_p_0[0])
+    print("P_omk:", result_p_omk[0])
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+from threading import Thread, Barrier
+
+epsilon = 1e-7
+barrier = Barrier(3)
+
+
+def func(x, result):
+    sum = 0
+    n = 0
+    term = 1
+    while abs(term) > epsilon:
+        sum += term
+        n += 1
+        term = (-1)**n * x**(2 * n) / math.factorial(n)
+    result.append(sum)
+    barrier.wait()
+
+
+def func2(x, result):
+    sum = 0
+    n = 1
+    while True:
+        term = 1 / (2 * n - 1) * ((x - 1) / (x + 1))**(2 * n - 1)
+        if abs(term) < epsilon:
+            break
+        else:
+            sum += term
+            n += 1
+    result.append(sum)
+    barrier.wait()
+
+
+def test(result1, result2):
+    barrier.wait()  # Ожидание, пока обе функции завершат свою работу, чтобы потом проверить правильность результатов
+    sum_func = result1[0]
+    sum_func2 = result2[0]
+
+    test1 = math.exp(-(-0.7)**2)
+    test2 = 1/2 * math.log(0.6)
+
+    print(f"Результат функции 1: {sum_func}")
+    print(f"Контрольное значение для функции 1: {test1}")
+    print(f"Результат функции 2: {sum_func2}")
+    print(f"Контрольное значение для функции 2: {test2}")
+
+    if abs(sum_func - test1) < epsilon:
+        print("func: Верно.")
+    else:
+        print("func: Неверно.")
+
+    if abs(sum_func2 - test2) < epsilon:
+        print("func2: Верно.")
+    else:
+        print("func2: Неверно.")
+
+
+def main():
+    result1 = []
+    result2 = []
+
+    thread1 = Thread(target=func, args=(-0.7, result1))
+    thread2 = Thread(target=func2, args=(0.6, result2))
+    thread3 = Thread(target=test, args=(result1, result2))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+
+
+if __name__ == "__main__":
+    main()
